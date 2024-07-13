ca02c39ceb7a6e3e312af4fa10bd64a3d7a1413b
Muhammad Ahmed <ahmedsidd_qui@outlook.com>
2024-07-13 20:46:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const existingDiv = document.querySelector(".circle");
+const fragment = document.createDocumentFragment();
+
+
+for (let i = 0; i < 280; i++) {
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("circle");
+
+  fragment.appendChild(newDiv); 
+}
+
+existingDiv.parentNode.insertBefore(fragment, existingDiv.nextSibling);
+  const coords = { x: 0, y: 0 };
+  const circles = document.querySelectorAll(".circle");
+
+  circles.forEach(function (circle) {
+    circle.x = 0;
+    circle.y = 0;
+  });
+
+  window.addEventListener("mousemove", function (e) {
+    coords.x = e.clientX;
+    coords.y = e.clientY;
+  });
+
+  function animateCircles() {
+    let x = coords.x;
+    let y = coords.y;
+
+    circles.forEach(function (circle, index) {
+      circle.style.left = x - 12 + "px";
+      circle.style.top = y - 12 + "px";
+
+      circle.style.scale = (circles.length - index) / circles.length;
+
+      circle.x = x;
+      circle.y = y;
+
+      const nextCircle = circles[index + 1] || circles[0];
+      x += (nextCircle.x - x) * 0.1;
+      y += (nextCircle.y - y) * 0.1;
+    });
+
+    requestAnimationFrame(animateCircles);
+    
+  }
+  
+
+  animateCircles();
